5ecdae01abe61c7b464e9593f8adb31213b7f236
Jakub <daicheq@daicheq.pl>
2022-06-21 15:19:15 +0200

    added gallery in aboutus section

---
+++ js/slick.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+	$('.aboutus-gallery').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2500,
+		arrows: false,
+		mobileFirst: true,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	})
+})
